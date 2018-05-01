8e01d6e11fe1685010f071920ead8ce9fb94666a
amgranad <ambergkim@gmail.com>
2018-04-30 19:27:27 -0700

    fixed delete and update issue always just deleting or updating the very first item on the list

--- lab-amber/src/components/note-item.jsx
+++ lab-amber/src/components/note-item.jsx
@@ -21,9 +21,8 @@ class Note extends React.Component{
   }
 
   deleteNote(event) {
-    console.log('delete note x clicked');
     event.preventDefault();
-    this.props.removeNote(this.props.id);
+    this.props.removeNote(event.target.id);
   }
 
   finishUpdate(noteInfo) {
@@ -40,7 +39,7 @@ class Note extends React.Component{
       return <NoteUpdateForm title={this.props.title} content={this.props.content} id={this.props.id} key={this.props.id} finishUpdate={this.finishUpdate} cancelUpdate={this.cancelUpdate}></NoteUpdateForm>
     }
     return <li onDoubleClick={this.toggleEdit}>
-      {this.props.title}: {this.props.content}. <a onClick={this.deleteNote}>x</a>
+      {this.props.title}: {this.props.content}. <a onClick={this.deleteNote} id={this.props.id}>x</a>
     </li>
   }
 }
